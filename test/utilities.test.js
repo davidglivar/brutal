b1a42f23be26e94638b6ec1fdeaad53c9aba813b
David Glivar <davidglivar@gmail.com>
2014-03-24 18:05:03 -0700

    the brutality continues

--- test/utilities.test.js
+++ test/utilities.test.js
@@ -34,4 +34,25 @@ describe('utilities', function () {
       expect(obj.foo).to.be.ok();
     });
   });
+
+  describe('#key()', function () {
+    
+    it('is a function with an arity of 1', function () {
+      expect(typeof _.key).to.be('function');
+      expect(_.key.length).to.be(1);
+    });
+
+    it('returns an object', function () {
+      var key = _.key('/path/to/an/image.png');
+      expect(key).to.be.ok();
+      expect(typeof key).to.be('object');
+    });
+
+    it('normalizes a filename without extention or retina id', function () {
+      expect(_.key('/path/to/an/image@2x.png')).to.eql({ key: 'image', retina: true });
+      expect(_.key('/path/image-2x.png')).to.eql({ key: 'image', retina: true });
+      expect(_.key('/path/image_2x.png')).to.eql({ key: 'image', retina: true });
+      expect(_.key('/path/image.png')).to.eql({ key: 'image', retina: false });
+    });
+  });
 });
