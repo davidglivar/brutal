e704d5599358fab378a9540f2694b13bf34ac824
David Glivar <davidglivar@gmail.com>
2014-04-03 16:27:16 -0700

    group tests

--- _brutal.js
+++ _brutal.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var brutal = require('./brutal').init({
   output: path.join(__dirname, 'test_output'),
+  margin: 2,
   padding: 2
 });
 brutal.run(path.join(__dirname, '/test/images'));
--- test/group.test.js
+++ test/group.test.js
@@ -1,11 +1,28 @@
 var expect = require('expect.js')
+  , _ = require('../lib/utilities')
+  , brutal = require('../brutal')
   , Group = require('../lib/group');
 
 describe('Group', function () {
-  it('write a test');
+  
+  it('is a function with an arity of 1', function () {
+    expect(Group).to.be.a('function');
+    expect(Group.length).to.be(1);
+  });
+
+  it('augments EventEmitter', function () {
+    var g = new Group('./test/images/bucket_a');
+    expect(g.on).to.be.a('function');
+    expect(g.emit).to.be.a('function');
+  });
 
   describe('VALIDATION_MESSAGE', function () {
-    it('write a test');
+    
+    it('is immutable', function () {
+      var value = Group.VALIDATION_MESSAGE;
+      Group.VALIDATION_MESSAGE = 'test';
+      expect(Group.VALIDATION_MESSAGE).to.be(value);
+    });
   });
 
   describe('#draw()', function () {
@@ -13,6 +30,39 @@ describe('Group', function () {
   });
 
   describe('#validate()', function () {
-    it('write a test');
+    
+    it('is a function', function () {
+      expect(Group.prototype.validate).to.be.a('function');
+    });
+
+    it('passes with a directory containing an even number of images', function () {
+      var len = brutal.validations.length
+        , g = new Group('./test/images/bucket_a');
+      expect(brutal.validations.length).to.be(len);
+    });
+
+    it('fails with a directory containing an odd number of images', function () {
+      var len = brutal.validations.length
+        , g = new Group('./test/images/bucket_a');
+      g.files.pop();
+      g.validate();
+      expect(brutal.validations.length).to.be(len+1);
+    });
+
+    it('passes with a directory containing matching sprites', function () {
+      var len = brutal.validations.length
+        , g = new Group('./test/images/bucket_a');
+      expect(brutal.validations.length).to.be(len);
+    });
+
+    it('fails with a directory containing mismatched sprites', function () {
+      var len = brutal.validations.length
+        , g = new Group('./test/images/invalid')
+        , msg = _.template(Group.VALIDATION_MESSAGE, {
+            key: g.sheets.standard.key,
+            msg: 'Sprite pair with key "'+g.sheets.standard.sprites[0].key+'" do not match'
+          });
+      expect(brutal.validations.indexOf(msg)).to.not.be(-1);
+    });
   });
 });
