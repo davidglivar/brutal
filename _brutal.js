e5554f88f210782c981114f517389c5e5e414c74
David Glivar <davidglivar@gmail.com>
2014-04-03 11:23:29 -0700

    padding is figured out?

--- _brutal.js
+++ _brutal.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var brutal = require('./brutal').init({
-  output: path.join(__dirname, 'test_output') //,
-  //padding: 2
+  output: path.join(__dirname, 'test_output'),
+  padding: 2
 });
 brutal.run(path.join(__dirname, '/test/images'));
 
